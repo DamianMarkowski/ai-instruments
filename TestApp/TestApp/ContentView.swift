e3c9c81796fadc42eafc0bce1d8888ae23973f50
Damian Markowski <contact@damianmarkowski.com>
2026-02-14 23:34:34 +0100

    test iOS app added

---
+++ TestApp/TestApp/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  TestApp
+//
+//  Created by Damian Markowski on 14/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Test app")
+    }
+}
+
+#Preview {
+    ContentView()
+}
